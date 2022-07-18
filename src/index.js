3cac89ffdea824e8d0e99d2400a1653cd569cf84
Miriam Torrens <m.torrens14@gmail.com>
2022-07-18 11:43:05 +0200

    import HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 //import { DndProvider } from 'react-dnd';
 //import { HTML5Backend } from 'react-dnd-html5-backend';
 import "./index.css";
